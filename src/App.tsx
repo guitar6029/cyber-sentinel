7443c0479ccb0d2f3fa39819e7496f46d3b1d1b3
Josh <jsbusiness305@gmail.com>
2025-01-22 17:45:19 -0500

    git init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Routes, Route} from 'react-router-dom'
+import Landing from './pages/Landing'
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  )
+}
+
+export default App
